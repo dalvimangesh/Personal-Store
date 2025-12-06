964d0a8924e254d88fb014ab2068033746f0baa0
Mangesh Dalvi <dalvimangesh000@gmail.com>
2025-12-06 19:53:06 +0900

    Fix import card bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
-import { CardTitle, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Card, CardTitle, CardDescription, CardFooter, CardHeader, CardContent } from "@/components/ui/card";
 import { ModeToggle } from "@/components/ModeToggle";
 
 export default function LoginPage() {
